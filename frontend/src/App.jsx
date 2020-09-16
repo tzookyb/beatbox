d6e25ce28a2159408cfce2875c7612266a976339
hilla <hilla7070@gmail.com>
2020-09-16 22:20:57 +0300

    git dofek acol

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,64 +9,32 @@ import { BoxDetails } from './pages/BoxDetails';
 import { BoxAdd } from './pages/BoxAdd';
 import { Home } from './pages/Home';
 import { UserDetails } from './pages/UserDetails';
-
-
 class _App extends Component {
   state = {
-
   }
-
   componentDidMount() {
   }
-
   render() {
     return (
-<<<<<<< HEAD
       <div className="App">
         <Header />
         <main>
           <Switch>
-            <Route component={BoxEdit} path="/box/edit/:id?" />
+            <Route component={BoxAdd} path="/box/add" />
             <Route component={BoxDetails} path="/box/:boxId" />
             <Route component={UserDetails} path="/user/:id" />
             <Route component={BoxApp} path="/box" />
             <Route component={Home} path="/" />
           </Switch>
         </main>
-=======
-      <div id="page-top" className="main-container">
-        <Player></Player>
-        {/* <Header />
-        <Switch>
-<<<<<<< HEAD
-        <Route component={BoxAdd} path="/box/add/"/>
-        <Route component={BoxDetails} path="/box/:id"/>
-        <Route component={UserDetails} path="/user/:id"/>
-        <Route component={BoxApp} path="/box"/>
-        <Route component={Home} path="/"/>
-        </Switch>
-=======
-          <Route component={BoxEdit} path="/box/edit/:id?" />
-          <Route component={BoxDetails} path="/box/:id" />
-          <Route component={UserDetails} path="/user/:id" />
-          <Route component={BoxApp} path="/box" />
-          <Route component={Home} path="/" />
-        </Switch> */}
->>>>>>> e14778ca0fcb5d6b64cca5bce99e1dcbce686dff
->>>>>>> 3f6d4f96feded7b30a6a9b76b2eac55060a61c2f
       </div>
     )
   }
 }
-
 const mapStateToProps = state => {
   return {
-
   }
 }
-
 const mapDispatchToProps = {
-
 }
-
 export const App = connect(mapStateToProps, mapDispatchToProps)(_App);
