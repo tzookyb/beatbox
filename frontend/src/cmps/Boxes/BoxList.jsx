630c0e15f4801c762f54a04dd5690329312c7f84
Matan Crispel <matan.cris@gmail.com>
2020-09-17 19:42:01 +0300

    thur ev

--- frontend/src/cmps/Boxes/BoxList.jsx
+++ frontend/src/cmps/Boxes/BoxList.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { AutoRotatingCarousel } from 'material-auto-rotating-carousel';
 
 import { BoxPreview } from './BoxPreview'
 
