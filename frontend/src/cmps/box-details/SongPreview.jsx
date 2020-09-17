ea9c0d0d57ba57ed0558145552d61749b3079a2a
tzookyb <tzookyb@gmail.com>
2020-09-17 13:31:23 +0300

    matan changes

--- frontend/src/cmps/box-details/SongPreview.jsx
+++ frontend/src/cmps/box-details/SongPreview.jsx
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 export function SongPreview({ song, onRemoveSong, onPlaySong }) {
     return (
         <li onClick={() => onPlaySong()} className="song-preview flex space-between" >
             <div className="song-data flex align-center">
-                <div className="song-preview-img"><img src={song.imgUrl.url} /></div>
+                <div className="song-preview-img"><img src={song.imgUrl.url} alt="song-img" /></div>
                 <h3>{song.title}</h3>
             </div>
             <div className="song-preview-btns flex align-center">
