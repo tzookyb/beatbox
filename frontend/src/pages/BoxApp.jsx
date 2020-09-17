9601324bf16c81cf86d80f2ef454078f6f428424
tzookyb <tzookyb@gmail.com>
2020-09-17 04:16:36 +0300

    removed hiding '0'

--- frontend/src/pages/BoxApp.jsx
+++ frontend/src/pages/BoxApp.jsx
@@ -24,7 +24,7 @@ class _BoxApp extends Component {
         if (!boxes || !genres) return <h1>Loading....</h1>
         return (
             <section className="box-app" id="box">
-                {genres.length && genres.map((genre, idx) => <BoxList boxes={boxes} key={idx} genre={genre} />)}
+                {genres.length || genres.map((genre, idx) => <BoxList boxes={boxes} key={idx} genre={genre} />)}
                 {!genres.length && <BoxList boxes={boxes} />}
             </section>
         )
