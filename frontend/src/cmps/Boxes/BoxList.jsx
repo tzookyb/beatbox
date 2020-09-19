b5c4a5f9835f10068256e9196a47b2446c936e78
tzookyb <tzookyb@gmail.com>
2020-09-19 16:42:02 +0300

    readability imporvements

--- frontend/src/cmps/Boxes/BoxList.jsx
+++ frontend/src/cmps/Boxes/BoxList.jsx
@@ -3,17 +3,13 @@ import { Link } from 'react-router-dom'
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 
-
-
 import { BoxPreview } from './BoxPreview'
 
-// const ref = 
-
-export function BoxList({ boxes, genre,onToggleLikeBox,minimalUser, onAddToFavorites }) {
+export function BoxList({ boxes, genre, onToggleLikeBox, minimalUser, onAddToFavorites }) {
     const ref = React.createRef()
 
 
-    const executeScroll = (scrollTo) =>{
+    const executeScroll = (scrollTo) => {
         let scrollDiff = ref.current.scrollWidth - ref.current.offsetWidth
         console.dir(ref)
 
@@ -22,30 +18,41 @@ export function BoxList({ boxes, genre,onToggleLikeBox,minimalUser, onAddToFavor
         console.log("executeScroll -> scrollTo", scrollTo)
         console.log("scrollToRef -> ref.current.scrollLeft", ref.current.scrollLeft)
         console.log("scrollToRef -> ref.current.scrollOffset", scrollDiff)
-    } 
+    }
 
-   
     if (!boxes) return <h1>Loading...</h1>
     return (
         <section className={`list-container ${genre ? '' : 'main-container'}`}>
-          
+
             {genre && <Link to={`/box?&genre=${genre}`} className="btn-genre">{genre}</Link>}
             {genre &&
 
                 <div ref={ref} className="box-list image-container">
-                    <button className="list-left-btn" onClick={() => executeScroll(-350)}><ArrowBackIosIcon/></button>
+                    <button className="list-left-btn" onClick={() => executeScroll(-350)}><ArrowBackIosIcon /></button>
                     {boxes.map(box => {
                         if (box.tags.includes(genre)) {
-                            return <BoxPreview isHomePage={true} key={box._id} box={box} genre={genre}
-                             onToggleLikeBox={onToggleLikeBox} onAddToFavorites={onAddToFavorites} minimalUser={minimalUser}/>
+                            return <BoxPreview
+                                isHomePage={true}
+                                key={box._id}
+                                box={box} genre={genre}
+                                onToggleLikeBox={onToggleLikeBox}
+                                onAddToFavorites={onAddToFavorites}
+                                minimalUser={minimalUser}
+                            />
                         } else return null
                     })}
-                    <button className="list-right-btn" onClick={() => executeScroll(350)}><ArrowForwardIosIcon/></button>
+                    <button className="list-right-btn" onClick={() => executeScroll(350)}><ArrowForwardIosIcon /></button>
                 </div>
             }
             <div className="box-list full-grid ">
-                {!genre && boxes.map(box => <BoxPreview isHomePage={false} key={box._id} box={box} minimalUser={minimalUser}
-                onAddToFavorites={onAddToFavorites}/>)}
+                {!genre && boxes.map(box => <BoxPreview
+                    isHomePage={false}
+                    key={box._id}
+                    box={box}
+                    minimalUser={minimalUser}
+                    onAddToFavorites={onAddToFavorites}
+                />
+                )}
             </div>
         </section>
     )
