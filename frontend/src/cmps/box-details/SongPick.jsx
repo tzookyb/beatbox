6415297395f8d3e142c01969de7d13e696f9822f
tzookyb <tzookyb@gmail.com>
2020-09-19 14:18:54 +0300

    regex and HTML codes removal, songpick styling

--- frontend/src/cmps/box-details/SongPick.jsx
+++ frontend/src/cmps/box-details/SongPick.jsx
@@ -1,9 +1,7 @@
 import React, { Component } from 'react'
-import { youtubeService } from '../../services/youtubeService';
 import { debounce } from 'debounce';
 
-
-
+import { youtubeService } from '../../services/youtubeService';
 
 export class SongPick extends Component {
     state = {
@@ -40,17 +38,19 @@ export class SongPick extends Component {
         const { results, isSearching, searchStr } = this.state;
 
         return (
-            <div className={`song-pick ${this.props.isSearchOpen ? 'opened':''}`}>
+            <div className={`song-pick ${this.props.isSearchOpen ? 'opened' : ''}`}>
                 <input type="search" name="searchStr" value={searchStr} onChange={this.handleInput} placeholder="Add song to playlist" autoComplete="off" />
 
                 {(isSearching && !results) && <div>Getting results...</div>}
 
                 {results && results.map(result => {
                     const id = result.id.videoId;
-                    const { title } = result.snippet;
+                    const title = youtubeService.titleSimplify(result.snippet.title);
                     const imgUrl = result.snippet.thumbnails.default.url;
+
                     return <div key={id} className="song-pick-result" onClick={() => this.onAddSong(result)}>
-                        <img src={imgUrl} alt="thumbnail" />{title}
+                        <img src={imgUrl} alt="thumbnail" />
+                        <span dir="auto">{title}</span>
                     </div>
                 })
                 }
--- frontend/src/services/youtubeService.js
+++ frontend/src/services/youtubeService.js
@@ -1,9 +1,12 @@
 import axios from 'axios';
+import he from 'he';
+
 const BASE_URL = 'https://www.googleapis.com/youtube/v3/search'
 const API_KEY = 'AIzaSyCPP5cxksnuliRKXkqCqeZYG3dviWGM5cM';
 
 export const youtubeService = {
-    get
+    get,
+    titleSimplify
 }
 
 async function get(query) {
@@ -14,4 +17,14 @@ async function get(query) {
     } catch (err) {
         console.dir(err);
     }
+}
+
+function titleSimplify(title) {
+    // Removes HTML char codes
+    let output = he.decode(title);
+    // Removes 'Official Video' brackets
+    const officialVideoRegex = /[([].?official.?video.?[)\]]/ig
+    output = output.replace(officialVideoRegex, '');
+    
+    return output;
 }
