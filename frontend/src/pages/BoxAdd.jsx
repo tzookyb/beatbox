684446bd2eca7a842d4a362a62b589ddf436ad26
tzookyb <tzookyb@gmail.com>
2020-10-16 16:24:24 +0300

    boxAdd changes

--- frontend/src/pages/BoxAdd.jsx
+++ frontend/src/pages/BoxAdd.jsx
@@ -3,11 +3,13 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
 import CircleLoading from 'react-loadingg/lib/CircleLoading'
+import CloseIcon from '@material-ui/icons/Close';
 // LOCAL IMPORTS
 import { boxService } from '../services/boxService'
 import { userService } from '../services/userService'
 import { saveBox, loadBoxes } from '../store/actions/boxAction'
 import { BoxInfoEdit } from '../cmps/box-details/BoxInfoEdit'
+const defaultBoxImg = 'https://res.cloudinary.com/tzookyb/image/upload/v1602846820/beatbox/j8kqknrb7vkjhzh8muzj.jpg'
 
 export class _BoxAdd extends Component {
     state = {
@@ -61,7 +63,9 @@ export class _BoxAdd extends Component {
             this.printMsg('Genre of box is required');
             return;
         }
-
+        if (!this.state.editBox.imgUrl) {
+            await this.setState({ editBox: {...this.state.editBox, imgUrl: defaultBoxImg}});
+        }
         const addedBox = await this.props.saveBox(this.state.editBox);
         this.props.loadBoxes();
         this.props.history.push(`/box/details/${addedBox._id}`);
@@ -74,24 +78,27 @@ export class _BoxAdd extends Component {
 
     render() {
         const { editBox } = this.state;
+        const msg = this.state.msgWarning;
         if (!editBox) return <CircleLoading size="large" color="#ac0aff" />
+
         return (
             <section onClick={this.closeModal} className="box-add-container flex justify-center">
-                
-                <div className="box-add-modal flex column" onClick={ev => ev.stopPropagation()}>
 
+                <div className="box-add-modal flex column" onClick={ev => ev.stopPropagation()}>
+                    <CloseIcon className="close-btn cursor-pointer" onClick={this.closeModal} />
                     <h2>Create your own Box</h2>
+
 
                     <BoxInfoEdit updateBox={this.updateBox} setIsLoading={this.setIsLoading} />
 
-                        <button
-                            disabled={this.state.isLoading}
-                            className={`btn-create ${this.state.isLoading ? 'faded-btn' : ''}`}
-                            onClick={this.onAddBox}
-                        >
-                            Create Box
+                    <button
+                        disabled={this.state.isLoading}
+                        className={`btn-create ${this.state.isLoading ? 'faded-btn' : ''}`}
+                        onClick={this.onAddBox}
+                    >
+                        Create Box
                         </button>
-                        {this.state.msgWarning && <label className="msg-warnning">{this.state.msgWarning}</label>}
+                    <span className="msg-warning">{msg}</span>
                 </div>
             </section>
         )
