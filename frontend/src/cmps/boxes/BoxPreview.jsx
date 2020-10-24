73b8c3c76b95023565dcf4ec3adbbbcce20d2378
tzookyb <tzookyb@gmail.com>
2020-10-24 18:35:10 +0300

    change link to history

--- frontend/src/cmps/boxes/BoxPreview.jsx
+++ frontend/src/cmps/boxes/BoxPreview.jsx
@@ -1,11 +1,11 @@
 // OUTSOURCE IMPORTS
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import { withRouter } from 'react-router-dom'
 import LazyLoad from 'react-lazy-load';
 import DeleteIcon from '@material-ui/icons/Delete';
 import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
 
-export class BoxPreview extends Component {
+export class _BoxPreview extends Component {
     state = {
         isImgLoaded: false
     }
@@ -15,29 +15,35 @@ export class BoxPreview extends Component {
     }
 
     render() {
-        const { box, isHomePage, onDelete } = this.props;
-        const sectionClassName = `box-preview ${isHomePage ? 'box-home-preview' : ''}`;
+        const { box, isHomePage, onDelete, history } = this.props;
+        const sectionClassName = `box-preview cursor-pointer ${isHomePage ? 'box-home-preview' : ''}`;
         const imgClass = this.state.isImgLoaded ? '' : 'img-loading';
         return (
-            <section className={sectionClassName} title={box.name}>
-                <Link to={`/box/details/${box._id}`} >
-                    <LazyLoad debounce={false} offsetVertical={100} offsetHorizontal={100}>
-                        <div className="box-preview-img"> <img className={imgClass} src={box.imgUrl} onLoad={this.onImgLoad} alt="box-preview img" /></div>
-                    </LazyLoad>
-                </Link>
+            <section className={sectionClassName} title={box.name} onClick={() => history.push(`/box/details/${box._id}`)}>
+                <LazyLoad debounce={false} offsetVertical={100} offsetHorizontal={100}>
+                    <div className="box-preview-img">
+                        <img
+                            className={imgClass}
+                            src={box.imgUrl}
+                            onLoad={this.onImgLoad}
+                            alt="box-preview img" />
+                    </div>
+                </LazyLoad>
 
                 <div className="box-preview-details flex column">
                     <div className="box-data flex space-between">
                         <h3 className="box-name">{box.name}</h3>
-                        <Link to={`/box/details/${box._id}`} >
-                            <PlayCircleOutlineIcon />
-                        </Link>
+                        <PlayCircleOutlineIcon />
                     </div>
-                    <div className="delete-btn" title="Delete box">
+
+                    <div className="delete-btn cursor-pointer" title="Delete box">
                         {onDelete && <DeleteIcon onClick={(ev) => onDelete(ev, box._id)} />}
                     </div>
+
                 </div>
             </section >
         )
     }
 }
+
+export const BoxPreview = withRouter(_BoxPreview);
