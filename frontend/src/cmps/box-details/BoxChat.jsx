3bad5ed49dd9a03199d75f026487e828117b1daf
tzookyb <tzookyb@gmail.com>
2020-11-11 17:55:10 +0200

    improve

--- frontend/src/cmps/box-details/BoxChat.jsx
+++ frontend/src/cmps/box-details/BoxChat.jsx
@@ -1,24 +1,25 @@
 // OUTSOURCE IMPORTS
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { SystemMessage } from 'react-chat-elements'
 import { Input } from 'react-chat-elements'
 import { Button } from 'react-chat-elements'
 import Avatar from '@material-ui/core/Avatar';
 import AvatarGroup from '@material-ui/lab/AvatarGroup';
+import { SystemMessage } from 'react-chat-elements';
 import 'react-chat-elements/dist/main.css';
+
 
 // LOCAL IMPORTS
 import { socketService } from '../../services/socketService'
-import { addMsg, loadMsgs } from '../../store/actions/msgAction'
+import { addMsg } from '../../store/actions/msgActions'
 
 class _BoxChat extends Component {
     state = {
-        msgs: [],
         msg: '',
         isTyping: false,
         typingStr: '',
     }
+
     typingTimeout;
     inputRef = React.createRef();
     chatRef = React.createRef();
@@ -56,12 +57,7 @@ class _BoxChat extends Component {
         this.setState({ msg: ev.target.value });
     }
 
-    timeoutFunction = () => {
-        this.setState({ isTyping: false });
-        socketService.emit('chat typing', '');
-    }
-
-    sendMsg = (ev) => {
+    sendMsg = () => {
         const { msg } = this.state;
         if (msg) {
             const msgObj = {
@@ -75,48 +71,9 @@ class _BoxChat extends Component {
         }
         this.setState({ msg: '' })
         this.inputRef.clear();
-    };
-
-    getMsgsArr() {
-        const { msgs, user } = this.props;
-        const msgsArr = [];
-        msgs.forEach((msg, idx) => {
-            let classUser;
-            let classPosition = '';
-            let title;
-            if (msg.id === user._id) {
-                title = 'you';
-                classUser = 'user-message';
-                classPosition = 'flex-end'
-            } else {
-                title = msg.submitBy;
-                classUser = 'not-user-message';
-            }
-            const date = new Date(msg.submitAt);
-            const dateToString = `${date.getHours()}:${date.getMinutes().toString().padStart(2, '0')}`;
-            if (msg.id === 'system') {
-                msgsArr.push(<SystemMessage
-                    key={idx} text={msg.text} />)
-            }
-            else {
-                msgsArr.push(
-                    <div key={idx} className={`msg flex column ${classPosition}`} >
-                        <div className={`msg-data flex align-center`}>
-                            {!classPosition && <Avatar title={user.username} alt="User" src={msg.avatar} style={{ width: '20px', height: '20px' }} />}
-                            <p className="submit-by">{title}</p>
-                            {dateToString}
-                        </div>
-                        <div className={`msg-text ${classUser}`}>
-                            {msg.text}
-                        </div>
-                    </div>
-                )
-            }
-        })
-        return msgsArr;
     }
 
-    getConnectedAvatars() {
+    getAvatars() {
         const { connectedUsers } = this.props;
         return connectedUsers.map(user => {
             return <Avatar key={user.id} src={user.imgUrl} />
@@ -127,24 +84,57 @@ class _BoxChat extends Component {
         if (ev.keyCode === 13) this.sendMsg();
     }
 
+    formatMsgs(msg, user, idx) {
+        const isSystemMsg = msg.id === 'system';
+        if (isSystemMsg) {
+            return <SystemMessage key={idx} text={msg.text} />
+        } else {
+            const isUserMsg = msg.id === user._id;
+            const userClass = isUserMsg ? 'user-message' : 'other-user-message';
+            const positionclass = isUserMsg ? 'flex-end' : '';
+            const submitBy = isUserMsg ? 'you' : msg.submitBy;
+
+            const date = new Date(msg.submitAt);
+            const dateToString = `${date.getHours()}:${date.getMinutes().toString().padStart(2, '0')}`;
+            return (
+                <div key={idx} className={`msg flex column ${positionclass}`} >
+                    <div className={`msg-data flex align-center`}>
+                        {!positionclass && <Avatar title={user.username} alt="User" src={msg.avatar} style={{ width: '20px', height: '20px' }} />}
+                        <p className="submit-by">{submitBy}</p>
+                        {dateToString}
+                    </div>
+                    <div className={`msg-text ${userClass}`}>
+                        {msg.text}
+                    </div>
+                </div>
+            )
+        }
+    }
+
     render() {
+        var { msgs, user } = this.props;
         const { typingStr } = this.state;
-        const avatarsConnectedUser = this.getConnectedAvatars();
+        const connectedUsersAvatars = this.getAvatars();
+        msgs = msgs.map((msg, idx) => this.formatMsgs(msg, user, idx))
+
         return (
             <section className="wall-container flex column space-between">
                 <h2 className="chat-title">Box Wall</h2>
+
                 <div className="flex justify-center">
-                    < AvatarGroup max={4}>
-                        {avatarsConnectedUser}
+                    <AvatarGroup max={4}>
+                        {connectedUsersAvatars}
                     </AvatarGroup >
                 </div>
+
                 <div className="msgs">
-                    {this.getMsgsArr()}
+                    {msgs}
                     <div className="typing-container">
                         {typingStr && <h3>{typingStr}</h3>}
                     </div>
                     <div ref={this.chatRef}></div>
                 </div>
+
                 <div className="input-msg">
                     <Input
                         ref={el => (this.inputRef = el)}
@@ -166,16 +156,12 @@ class _BoxChat extends Component {
     }
 }
 
-const mapStateToProps = state => {
-    return {
-        user: state.userReducer.loggedinUser,
-        msgs: state.msgReducer.msgs,
-        connectedUsers: state.connectedUsersReducer.connectedUsers
-    }
-}
+const mapStateToProps = state => ({
+    user: state.userReducer.loggedinUser,
+    msgs: state.msgReducer.msgs,
+    connectedUsers: state.userReducer.connectedUsers
+})
 const mapDispatchToProps = {
-    loadMsgs,
     addMsg
 }
-
 export const BoxChat = connect(mapStateToProps, mapDispatchToProps)(_BoxChat)
